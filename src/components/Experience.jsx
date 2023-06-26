14a8b13d5d8f9e10e0dba1ceffc4491e3a181c48
joseleonn <josecaceresmusso@gmail.com>
2023-06-25 23:53:54 -0300

    s

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,7 +3,6 @@ const Experience = () => {
     <div className="h-full p-10 relative ">
       <h2 className="font-bold text-2xl">Experiencia</h2>
       <p className="mt-4">
-        Desarrollador Web Full Stack | Enero 2022 - Marzo 2023 FREELANCE
         Desarrollo de sitios web de manera freelance, enfocando el diseño en
         captación de usuarios, conversión de tráfico, UX/UI simple e intuitiva,
         y priorizando la optimización y SEO de la web. Trabajos realizados con
